7f6e378bf883c3de2933945c118d87f775d4cb6e
Gogo693 <crepaldigiorgio@hotmail.it>
2020-11-23 15:54:23 +0100

    first commit

---
+++ ACGPN_inference/command_test.sh
@@ -0,0 +1,3 @@
+nohup python train.py --dataroot ../../ACGPN_landmarks/ACGPN_train/data/ --gpu_ids 3 --name prova &
+nohup python test.py --dataroot data/Data_preprocessing --gpu_ids 0 &
+nohup python test.py --dataroot ../../ACGPN_landmarks_mesh/ACGPN_inference/data/Data_preprocessing/ --gpu_ids 3 --name prova &
---
+++ ACGPN_train/command_training.sh
@@ -0,0 +1,2 @@
+nohup python train.py --dataroot data --gpu_ids 3 --niter 10 --niter_decay 10 &
+nohup python train.py --dataroot ../../ACGPN_landmarks/ACGPN_train/data/ --gpu_ids 3 --niter 10 --niter_decay 10 --name custom &
